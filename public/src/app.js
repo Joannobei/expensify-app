1d335475d93692aaa7cbb3bac11e89bcd4f30843
JOAN <nobeijoan1.com>
2022-02-19 02:37:21 +0100

    Remove dummy data

--- public/src/app.js
+++ public/src/app.js
@@ -3,29 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { Provider } from 'react-redux' ;
-import {addExpense} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
+import { Provider } from 'react-redux';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
-import'normalize.css/normalize.css';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
- const store = configureStore();
+const store = configureStore();
 
-         store.dispatch(addExpense({ description: 'Water bill', amount : 500 }));
-         store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}));
-         store.dispatch(addExpense({ description: 'Rent', amount : 10500 }));
-        
-
-            const state = store.getState();
-            const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-            console.log(visibleExpenses);
-          
 const jsx = (
-        <Provider store = {store}>
+        <Provider store={store}>
                 <AppRouter />
         </Provider>
 )
-  ReactDOM.render(jsx, document.getElementById('app'));
-  
-    
+ReactDOM.render(jsx, document.getElementById('app'));
+
+
