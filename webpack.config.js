b7c7fee1798e60a2e0c6d9140f7fad293f4febf6
JOAN <nobeijoan1.com>
2022-02-21 10:13:11 +0100

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,57 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
 console.log(path.join(__dirname, 'public'));
 
-module.exports = {
-    entry: './public/src/app.js',
-    output: {
+module.exports = (env) => {
+    const isProduction = env === 'production';
 
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
 
-    },
+    console.log('env', env);
+    return {
+        entry: './public/src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js',
+        },
 
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$|jsx/,
-            exclude: /node_modules/,
-            options: { presets: ['@babel/env', '@babel/preset-react'] },
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-                ,]
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$|jsx/,
+                exclude: /node_modules/,
+                options: { presets: ['@babel/env', '@babel/preset-react'] },
+            }, {
+                test: /\.s?css$/,
+
+                use: [
+                    { loader: MiniCssExtractPlugin.loader, },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+
+            }]
+        },
+        plugins: [new MiniCssExtractPlugin()],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            static: path.join(__dirname, 'public'),
+            historyApiFallback: true,
         }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        static: path.join(__dirname, 'public'),
-        historyApiFallback: true,
-    }
-};
+    };
+}
+
+
 
 
